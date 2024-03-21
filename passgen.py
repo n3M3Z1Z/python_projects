2081aab9b46b174529a8a78588a16cf2f503432d
13n3m3z1z77 <157004134+mrH771386@users.noreply.github.com>
2024-03-21 12:40:38 +0100

    Add files via upload

---
+++ passgen.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#import section
+
+import random
+import string
+
+#Define and specify the function!
+
+def generate_password(length: int = 25):
+	alphabet = string.ascii_letters + string.digits + string.punctuation
+	password = ''.join(random.choice(alphabet) for i in range(length))
+	return password
+
+#Output Section
+
+password = generate_password()
+print(f"Generated password: {password}")
---
+++ whois.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#Import
+
+import socket
+
+#define the function
+
+def whois_lookup(domain: str):
+	s = socket.socket(socket.AF_INET,
+	socket.SOCK_STREAM)
+	s.connect(("whois.iana.org", 43))
+	s.send(f"{domain}\r\n".encode())
+	response = s.recv(4096).decode()
+	s.close()
+	return response
+
+#Example usage
+
+print(whois_lookup("heidees.de"))
